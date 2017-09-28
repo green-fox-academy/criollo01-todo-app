3a4d48e2d34974a5b5c02b01f02f60a8165b919f
Criollo01 <lucanagy@protonmail.com>
2017-09-28 11:46:24 +0200

    feat(display): create help_display function, printing the app usage text

--- todo_app.py
+++ todo_app.py
@@ -5,3 +5,23 @@ class Controller(object):
     help_text = Display()
     if len(sys.argv) == 1:
         help_text.help_display()
+
+
+
+
+
+
+
+class Display(object):
+    def help_display(self):
+        return print(
+                    "Python Todo application\n"
+                    "=======================\n"
+                    "\n"
+                    "Command line arguments:\n"
+                    "-l   Lists all the tasks\n"
+                    "-a   Adds a new task\n"
+                    "-r   Removes a task\n"
+                    "-c   Completes a task)\n")
+
+)
