37df91d292c876d271b1f507a5bb8db72779a9d6
Criollo01 <lucanagy@protonmail.com>
2017-09-28 16:00:16 +0200

    feat(Model): can add tasks

--- todo_app.py
+++ todo_app.py
@@ -5,7 +5,7 @@ class Controller():
     def get_argument(self):
         help_text = Display()
         if len(sys.argv) <= 1:
-            help_text.help_display()
+            help_text.print_usage()
         list_todo = Display()
         if sys.argv[1] == "-l":
             list_todo.list_display()
@@ -13,28 +13,30 @@ class Controller():
             if sys.argv[2:] == []:
                 print("Unable to add: no task provided.")
             else:
-                add_todo = Action()
+                add_todo = Model()
                 add_todo.add_task()
                 list_todo.list_display()
         elif sys.argv[1] == "-r":
             if sys.argv[2:] == []:
                 print("Unable to remove: no index provided")
             else:
-
+                pass
         elif sys.argv[1] == "-c":
             if sys.argv[2:] == []:
                 print("Unable to check: no index provided")
             else:
-
+                pass
         else:
             print("Unsupported argument")
-            help_display.help_text()
+            print_usage.help_text()
 
 
 class Display():
 
-    def help_display(self):
+    def print_usage(self):
         print(
+            "\n"
+            "\n"
             "Python Todo application\n"
             "=======================\n"
             "\n"
@@ -45,7 +47,7 @@ class Display():
             "-c   Completes a task)\n")
 
     def list_display(self):
-        read_list = Action()
+        read_list = Model()
         read_list.read_file()
         if read_list.read_file() == []:
             print("No todos for today! :)")
@@ -54,25 +56,35 @@ class Display():
             for task in read_list.read_file():
                 serial += 1
                 if task[0] == "0":
-                    print(serial, "[ ]", " - ", task[1][:-1])
+                    print(serial, "[ ] - ", task[0])
                 elif task[0] == "1":
-                    print(serial, "[x]", " - ", task[1][:-1])
+                    print(serial, "[x] - ", task[0])
 
-class Actions():
+class Model():
 
     def read_file(self):
         read_todo = open("todo.txt", "r")
         read_lines_todo = read_todo.readlines()
         todo_list = []
-        for line in todo_lines:
+        for line in read_lines_todo:
             todo_list.append(line.split("\n"))
         read_todo.close()
         return todo_list
 
     def add_task(self):
         append_todo = open("todo.txt", "a")
-        check_text = " ".join(sys.argv[2:])
-        append_text = " " + check_text + "\n"
-        todo_file.writelines(append_text)
-        todo_file.close()
+        check_text = "".join(sys.argv[2:])
+        append_text = "" + check_text + "\n"
+        append_todo.writelines(append_text)
+        append_todo.close()
         print("Task added successfully.")
+
+    def remove_task(self):
+        pass
+
+    def check_task(self):
+        pass
+
+
+control = Controller()
+control.get_argument()
