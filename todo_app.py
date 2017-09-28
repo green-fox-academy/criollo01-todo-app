bc6121c0d1ee1dcfc6f5787c9f270b280962d6c9
Criollo01 <lucanagy@protonmail.com>
2017-09-28 12:08:14 +0200

    feat(Model): create read_file function with returning the todo list

--- todo_app.py
+++ todo_app.py
@@ -1,27 +1,41 @@
 import sys
-import os
 
-class Controller(object):
-    help_text = Display()
-    if len(sys.argv) == 1:
-        help_text.help_display()
+class Controller():
 
-
-
+    def get_argument(self):
+        help_text = Display()
+        if len(sys.argv) <= 1:
+            help_text.help_display()
 
 
 
 
-class Display(object):
+class Display():
+
     def help_display(self):
-        return print(
-                    "Python Todo application\n"
-                    "=======================\n"
-                    "\n"
-                    "Command line arguments:\n"
-                    "-l   Lists all the tasks\n"
-                    "-a   Adds a new task\n"
-                    "-r   Removes a task\n"
-                    "-c   Completes a task)\n")
+        print(
+            "Python Todo application\n"
+            "=======================\n"
+            "\n"
+            "Command line arguments:\n"
+            "-l   Lists all the tasks\n"
+            "-a   Adds a new task\n"
+            "-r   Removes a task\n"
+            "-c   Completes a task)\n")
 
-)
+    def list_display(self):
+
+
+
+class Model():
+
+    def read_file():
+        read_todo = open("todo.txt", "r")
+        read_lines_todo = read_todo.readlines()
+        todo_list = []
+        for line in todo_lines:
+            todo_list.append(line.split("\n"))
+        read_todo.close()
+        return todo_list
+
+    
